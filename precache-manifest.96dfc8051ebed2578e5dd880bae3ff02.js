672b96f3599d7fa77a15065eb6c82defc37926ff
Nicolas Neiman <nicolasneiman@NN.local>
2020-05-03 17:23:33 -0500

    Updates

---
+++ precache-manifest.96dfc8051ebed2578e5dd880bae3ff02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e890c1a5bc7686c593c7be3e63ab2a85",
+    "url": "/TodoListWithReact/index.html"
+  },
+  {
+    "revision": "be6dad1433830c8755c6",
+    "url": "/TodoListWithReact/static/css/main.34da5ccf.chunk.css"
+  },
+  {
+    "revision": "63582bacea409f8c0f84",
+    "url": "/TodoListWithReact/static/js/2.f7a1b7b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoListWithReact/static/js/2.f7a1b7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be6dad1433830c8755c6",
+    "url": "/TodoListWithReact/static/js/main.cadefb28.chunk.js"
+  },
+  {
+    "revision": "8dbabe4a48d046d4141d",
+    "url": "/TodoListWithReact/static/js/runtime-main.7b9f1918.js"
+  }
+]);
